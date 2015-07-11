2abed2ce461f9d757cb9ce7a06cc6e672b98bf6e
Andrés Argüelles <aarguelles@uninorte.edu.co>
2015-07-11 02:59:19 +0000

    firs commit

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2015-05-16 00:26:45 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2015-05-16T00:26:45+00:00	INFO 10.240.238.31	joomlafailure	Usuario no existe
